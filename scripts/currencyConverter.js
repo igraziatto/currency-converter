dec9d4b767774e4606d6a78e44e80773ebdd185f
Isis Graziatto <isis.graziatto@gmail.com>
2017-09-05 11:28:25 -0400

    changed the design pattern
    added more comments;
    changed the design pattern, refactored a few methods

--- scripts/currencyConverter.js
+++ scripts/currencyConverter.js
@@ -3,160 +3,186 @@
  * @author Isis Graziatto <https://github.com/igraziatto>
  * @version 1.0.0
  */
-function currencyConverter(){
-	var self = arguments[0],
+/** 
+*
+* @constructor 
+*
+*/
+function CurrencyConverter(){
+	 "use strict";
+
+	if (!(this instanceof CurrencyConverter)) {
+		throw new Error("CurrencyConverter needs to be called with the 'new' keyword");
+	}
+
+	this.self = arguments[0];
+	var apiURL = "http://api.fixer.io/latest?base=",
 		form,
-		currentAmount,
-		currentCurrency,
-		convertedAmount,
 		convertedCurrency,
-		disclaimer,
+		convertedAmount,
 		btnDisclaimer;
-	/**
-	 * Set a value to the input[name="converted-amount"].
-	 *
-	 * @param {string} val - the value to be setted.
-	 */
-	function setConvertedAmount(val){
-		convertedAmount.value = val;
-	}
-	/**
-	 * Set innerHTML in designated elements inside div.block-disclaimer.
-	 *
-	 * @param {string} rateName - the choosen rate name. Ex: "USD", "CAD", "EUR".
-	 * @param {string} input - the amount to be converted.
-	 * @param {string} rate - the choosen rate value.
-	 * @param {string} output - the converted amount.
-	 */
-	function setDisclaimer(rateName, input, rate, output){
-		disclaimer.querySelectorAll(".rate-name")[0].innerHTML = rateName;
-		disclaimer.querySelectorAll(".input")[0].innerHTML = input;
-		disclaimer.querySelectorAll(".output")[0].innerHTML = output;
+}
+/**
+*
+* Contructor function, gather elements and call addEvents for plugin to work properly.
+*
+*/
+CurrencyConverter.prototype.init = function(){
+	//gather properties
+	form = this.self.querySelectorAll("[name='currency-form']")[0];
+	convertedCurrency = form.querySelectorAll("[name='converted-currency']")[0];
+	convertedAmount = form.querySelectorAll("[name='converted-amount']")[0];
+	btnDisclaimer = this.self.querySelectorAll(".btn-disclaimer")[0];
 
-		disclaimer.querySelectorAll(".rate-value").forEach(function(element){
-		  element.innerHTML = rate;
-		});
-	}
-	/**
-	 * Makes an AJAX call to http://api.fixer.io/ sending base as an parameter. Returns the data.
-	 *
-	 * @param {string} base - the choosen rate name. Ex: "USD", "CAD", "EUR".
-	 * @param {function} callback - a function to be called if the AJAX call is successful.
-	 */
-	function getRates(base, callback){
-		var request = new XMLHttpRequest();
-		request.open("GET", "http://api.fixer.io/latest?base="+base, true);
+	//call function to add events
+	this.addEvents();
+}
+/**
+* TODO
+*
+* @param {string} xxx - xxx
+*/
+CurrencyConverter.prototype.destroy = function(){
+	console.log("destroy method...")
+}
+/**
+*
+* Add "change" to convertedCurrency select, "submit" to form and "click" to btnDisclaimer 
+*
+*/
+CurrencyConverter.prototype.addEvents = function(){
+	//Get values from form and fire submit event
+	convertedCurrency.addEventListener("change", function(){
+		//Simulate form submit
+		this.self.querySelectorAll("[name='btn-submit']")[0].click();
+	}.bind(this), false);
 
-		request.onload = function(){
-		  if(this.status >= 200 && this.status < 400){
-		    // Success!
-		    var data = JSON.parse(this.response);
-		    callback(data);
-		  }else{
-			console.log("Error", "Reached target server, but it returned an error");
-			console.log("Error", request.responseText);
-		  }
-		};
-
-		request.onerror = function(){
-			console.log("Error", "An connection error occurred during the transaction");
-			console.log("Error", request.responseText);
-		};
-
-		request.send();
-	}
-	/**
-	 * Calculate an given amount against a given rate. Returns the result.
-	 *
-	 * @param {string} amount - The initial amount.
-	 * @param {string} rate - The rate.
-	 */
-	function calculateAmount(amount, rate){
-		return parseFloat(rate) / parseFloat(amount);
-	}
-	/**
-	 * Represents a book.
-	 *
-	 * @param {string} title - The title of the book.
-	 */
-	function submitCurrencyForm(){
-		getRates(form.values.currentCurrency, function(data){
-			console.log(data);
-			var ca = calculateAmount(form.values.currentAmount, data.rates[form.values.convertedCurrency]);
-			setConvertedAmount(ca);
-			setDisclaimer(form.values.convertedCurrency, form.values.currentAmount, data.rates[form.values.convertedCurrency], ca);
-		});
-	}
-	/**
-	 * Toggle the style visibility and opacity in a given element.
-	 *
-	 * @param {DOM object} element - the designated element.
-	 */
-	function toggleVisibility(element){
-		if(element.style.visibility === "hidden"){
-			element.style.opacity = 1;
-			element.style.visibility = "visible";
-		}else{
-			element.style.opacity = 0;
-			element.style.visibility = "hidden";
-		}
-	}
-	function addEvents(){
-		// Validate form logic before submiting
-		form.addEventListener("submit", function(e){
-			e.preventDefault();
-
-			//if amount entered is equal to 0, converted amount should be 0
-			if(form.values.currentAmount === "0"){
-				setConvertedAmount(0);
-				return false;
-			}
-			//if currencies are the same, there is no need for conversion
-			if(form.values.currentCurrency === form.values.convertedCurrency){
-				setConvertedAmount(form.values.currentAmount);
-				return false;
-			}
-			submitCurrencyForm();
-		});
-
-		//btnDisclaimer controls disclaimer visibility
-		btnDisclaimer.addEventListener("click", function(e){
-			toggleVisibility(disclaimer);
-		});
-
-		//get values from form and fire form submit event
-		convertedCurrency.addEventListener("change", function(){
-			form.values = {
-				currentAmount: currentAmount.value,
-				currentCurrency: currentCurrency.value,
-				convertedCurrency: convertedCurrency.value
+	//Validate form logic before submiting
+	form.addEventListener("submit", function(e){
+		e.preventDefault();
+		var form = this.self.querySelectorAll("[name='currency-form']")[0],
+			inputValues = {
+				currentAmount: form.querySelectorAll("[name='current-amount']")[0].value,
+				currentCurrency: form.querySelectorAll("[name='current-currency']")[0].value,
+				convertedCurrency: form.querySelectorAll("[name='converted-currency']")[0].value
 			};
-			btnSubmit.click();
-		});
-	}
-	/** 
-	*
-	* @constructor 
-	*
-	*/
-	function init(){
-		//gather properties
-		form = self.querySelectorAll("[name='currency-form']")[0];
-		currentAmount = form.querySelectorAll("[name='current-amount']")[0];
-		currentCurrency = form.querySelectorAll("[name='current-currency']")[0];
-		convertedAmount = form.querySelectorAll("[name='converted-amount']")[0];
-		convertedCurrency = form.querySelectorAll("[name='converted-currency']")[0];
-		btnSubmit = form.querySelectorAll("[name='btn-submit']")[0];
-		disclaimer = form.querySelectorAll(".disclaimer")[0];
-		btnDisclaimer = form.querySelectorAll(".btn-disclaimer")[0];
 
-		//call function to add events
-		addEvents();
-	}
+		//if amount entered is equal to 0, converted amount should be 0
+		if(inputValues.currentAmount === "0"){
+			this.setConvertedAmount(0);
+			return false;
+		}
+		//if currencies are the same, there is no need for conversion
+		if(inputValues.currentCurrency === inputValues.convertedCurrency){
+			this.setConvertedAmount(inputValues.currentAmount);
+			return false;
+		}
+		//submitCurrencyForm.call(this);
+		this.submitCurrencyForm(inputValues);
+	}.bind(this), false);
 
-	return {
-		self : self,
-		init : init,
-		calculateAmount: calculateAmount
+	//btnDisclaimer controls disclaimer visibility
+	btnDisclaimer.addEventListener("click", function(e){
+		var targetSelector = this.getAttribute('data-target'),
+			target = this.offsetParent.querySelectorAll(targetSelector)[0];
+		
+		toggleVisibility(target);
+	});
+}
+/**
+* Calculate an given amount against a given rate. Returns the result.
+*
+* @param {string} amount - The initial amount.
+* @param {string} rate - The rate.
+*/
+CurrencyConverter.prototype.calculateAmount = function(amount, rate){
+	return parseFloat(rate) / parseFloat(amount);
+}
+/**
+* Makes an AJAX call to http://api.fixer.io/ sending "base" as a parameter. Call callback sending "data" as parameter.
+*
+* @param {string} base - the choosen rate name. Ex: "USD", "CAD", "EUR".
+* @param {function} callback - a function to be called if the AJAX call is successful.
+*/
+CurrencyConverter.prototype.getRates = function(base, callback){
+	var request = new XMLHttpRequest();
+	request.open("GET", "http://api.fixer.io/latest?base="+base, true);
+
+	request.onload = function(){
+	  if(this.status >= 200 && this.status < 400){
+	    // Success!
+	    var data = JSON.parse(this.response);
+	    callback(data);
+	  }else{
+		console.log("Error", "Reached target server, but it returned an error");
+		console.log("Error", request.responseText);
+	  }
 	};
+
+	request.onerror = function(){
+		console.log("Error", "An connection error occurred during the transaction");
+		console.log("Error", request.responseText);
+	};
+
+	request.send();
+}
+/**
+* Set a value to the input[name="converted-amount"].
+*
+* @param {string} val - the value to be setted.
+*/
+CurrencyConverter.prototype.setConvertedAmount = function(val){
+	this.self.querySelectorAll("[name='converted-amount']")[0].value = val;
+}
+/**
+* Set innerHTML in designated elements inside div.block-disclaimer.
+*
+* @param {string} rateName - the choosen rate name. Ex: "USD", "CAD", "EUR".
+* @param {string} input - the amount to be converted.
+* @param {string} rate - the choosen rate value.
+* @param {string} output - the converted amount.
+*/
+CurrencyConverter.prototype.setDisclaimer = function(rateName, input, rate, output){
+	var disclaimer = this.self.querySelectorAll(".disclaimer")[0];
+
+	disclaimer.querySelectorAll(".rate-name")[0].innerHTML = rateName;
+	disclaimer.querySelectorAll(".input")[0].innerHTML = input;
+	disclaimer.querySelectorAll(".output")[0].innerHTML = output;
+
+	disclaimer.querySelectorAll(".rate-value").forEach(function(element){
+	  element.innerHTML = rate;
+	});
+}
+/**
+* Perform submit in the form, call getRates() with a callback function that call setConvertedAmount() and setDisclaimer()
+* @param {object} inputValues - object with values gathered from the form
+*/
+CurrencyConverter.prototype.submitCurrencyForm = function(inputValues){
+	this.getRates(inputValues.currentCurrency, function(data){
+		console.log(data);
+		var ca = this.calculateAmount(inputValues.currentAmount, data.rates[inputValues.convertedCurrency]);
+		this.setConvertedAmount(ca);
+		this.setDisclaimer(inputValues.convertedCurrency, inputValues.currentAmount, data.rates[inputValues.convertedCurrency], ca);
+	}.bind(this));
+};
+
+/**
+* ----------------
+* Helper Functions (Global)
+* -----------------
+*/
+
+/**
+* Toggles the style visibility and opacity in a given element.
+*
+* @param {DOM object} element - the designated element.
+*/
+toggleVisibility = function(element){
+	if(element.style.visibility === "hidden"){
+		element.style.opacity = 1;
+		element.style.visibility = "visible";
+	}else{
+		element.style.opacity = 0;
+		element.style.visibility = "hidden";
+	}
 }
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,13 +2,13 @@
 "use strict";
 
 window.onload = function(){
-	var currencyConverter1 = new currencyConverter(document.getElementById("currencyConverter1"));
+	var currencyConverter1 = new CurrencyConverter(document.getElementById("currencyConverter1"));
 	currencyConverter1.init();
 
-	var currencyConverter2 = new currencyConverter(document.getElementById("currencyConverter2"));
+	var currencyConverter2 = new CurrencyConverter(document.getElementById("currencyConverter2"));
 	currencyConverter2.init();
 
-	var currencyConverter3 = new currencyConverter(document.getElementById("currencyConverter3"));
+	var currencyConverter3 = new CurrencyConverter(document.getElementById("currencyConverter3"));
 	currencyConverter3.init();
 }
 
